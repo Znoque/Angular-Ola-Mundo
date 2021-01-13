8e24179c856480042148492acf19a1a9e8da9787
Enoque Teixeira <znoque5@gmail.com>
2021-01-13 11:03:40 -0300

    Alteração do Title

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Angular-Ola-Mundo app is running!');
+    expect(page.getTitleText()).toEqual('Olá Mundo!');
   });
 
   afterEach(async () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Ola-Mundo';
+  title = 'Olá Mundo!';
 }
